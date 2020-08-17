d2a14476dd24ad79678970313d71649c9fb8cffb
kaveenakash <kaveenakashya@gmail.com>
2020-08-17 12:51:51 -0700

    Intial WishList Commit

---
+++ app/src/main/java/com/example/catalogue/WishListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.catalogue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WishListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish_list_view);
+    }
+}
