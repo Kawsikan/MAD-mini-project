87c0a4306f1c34c4197998fbff9a016601ea2e97
kawsikan <kawsinote@gmail.com>
2020-08-17 09:17:14 +0530

    Third task commit

---
+++ app/src/main/java/com/example/catalogue/TaskListActivity.java
@@ -0,0 +1,14 @@
+package com.example.catalogue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
