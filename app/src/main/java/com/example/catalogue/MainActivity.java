2a68ac9036734e6610d36d5e0d31ae927eaf0db3
kawsikan <kawsinote@gmail.com>
2020-08-21 11:59:18 +0530

    Added onclick to login and google signin.

--- app/src/main/java/com/example/catalogue/MainActivity.java
+++ app/src/main/java/com/example/catalogue/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.catalogue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        // Toast.makeText(MainActivity.this,  editName.getText(), Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Welcome to login", Toast.LENGTH_SHORT).show();
+
+        startActivity(intent);
     }
 }
